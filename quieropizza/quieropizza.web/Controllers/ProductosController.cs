3e2b58cd26d05fb7ceec502bba8389f1be91ff8c
samuel suazo <alejandro.mendez@unah.hn>
2021-06-06 21:35:45 -0600

    agregando lista productos

---
+++ quieropizza/quieropizza.web/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using quieropizza.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quieropizza.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto = new ProductoModel();
+            producto.Id = 101;
+            producto.descripcion = "pizza hawaina";
+
+            return View(producto);
+        }
+    }
+}
